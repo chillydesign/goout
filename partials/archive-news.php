fcbb0ac87fae568c374ad6d5a436ab7fbede792f
Mélissa Rissel <rissel.melissa@gmail.com>
2017-12-12 10:13:32 +0000

    we

--- partials/archive-news.php
+++ partials/archive-news.php
@@ -1,9 +1,43 @@
 
 
 <?php $pp = 0; if (have_posts()):  ?>
+    <?php while (have_posts()) : the_post(); ?>
+
+  <?php if ($pp == 0){ ?>
+    <div class="latest_articles">
+            <div class="row">
+                <div class="col-sm-8 larger_article_image">
+
+                    <div class="latest_article">
+                      <?php $image = ( has_post_thumbnail()) ? thumbnail_of_post_url(get_the_ID(),  'medium') : ''; ?>
+                      <a  class="latest_image" href="<?php the_permalink(); ?>" style="background-image:url('<?php echo $image; ?>');" title="<?php the_title(); ?>"></a>
+                      <div class="latest_text">
+
+                          <h2><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
+
+                          <?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
+
+                      </div>
+                    </div>
+                <div class="col-sm-4 ">
+
+                                    </div>
+
+            </div>
+
+            <div class="row">
+                <div class="col-sm-4"></div>
+                <div class="col-sm-4"></div>
+                <div class="col-sm-4"></div>
+            </div>
+
+
+        </div>
+  <?php }  else {?>
+
     <div class="latest_articles">
         <div class="row">
-            <?php while (have_posts()) : the_post(); ?>
+
 
                 <div class=" latest_article col-sm-4">
                     <!-- article -->
@@ -25,10 +59,13 @@
                 </div>
                 <?php if ($pp % 3 == 2)  echo '</div><!--  END OF ROW --><div class="row">'; ?>
 
-                <?php $pp++; endwhile; ?>
+
+
         </div><!--  END OF ROW -->
     </div> <!-- END OF LATEST ARTICLES -->
 
+<?php }  ?>
+  <?php $pp++; endwhile; ?>
 <?php else: ?>
 
     <!-- article -->
--- taxonomy-news_cat.php
+++ taxonomy-news_cat.php
@@ -1,14 +1,22 @@
 <?php get_header(); ?>
+<?php // $posts_array =  convert_events_to_strings();  ?>
+
+
+
 
 <div class="container">
 
-
-    <h1 class="fancy_title">On en <strong><em>parle</em></strong>.</h1>
+  <h1 class="fancy_title">On en <strong><em>parle</em></strong>.</h1>
     <?php get_template_part('partials/categories-news'); ?>
     <?php get_template_part('partials/archive-news'); ?>
     <?php get_template_part('pagination'); ?>
 
+
 </div><!--  END OF CONTAINER -->
+
+
+
+    <?php // get_template_part('partials/events', 'index'); ?>
 
 
 
