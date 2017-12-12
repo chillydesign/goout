4b59bb990b4c02403b0a690867eed4c72304043c
Charles Harvey <harvey.charles@gmail.com>
2017-12-12 10:35:31 +0000

    pagination style change; front coolthings link change

--- partials/front-coolthings.php
+++ partials/front-coolthings.php
@@ -19,12 +19,12 @@ $coolthingarchivelink  = get_post_type_archive_link( 'coolthing' );
                     <?php $col_class = ( $c == 0 ) ? 'col-sm-8' : 'col-sm-4' ?>
                     <div class="coolthing_article <?php echo $col_class; ?>">
                         <?php $image = ( has_post_thumbnail()) ? thumbnail_of_post_url(get_the_ID(),  'large') : ''; ?>
-                        <?php $url = get_field('link'); ?>
+                        <?php $url =  get_the_permalink();  //esc_url(get_field('link')); ?>
                         <?php $title = get_the_title() ;?>
-                        <a  class="latest_image" href="<?php echo esc_url($url); ?>" style="background-image:url('<?php echo $image; ?>');" title="<?php echo $title; ?>">
+                        <a  class="latest_image" href="<?php echo ($url); ?>" style="background-image:url('<?php echo $image; ?>');" title="<?php echo $title; ?>">
                         </a>
                         <div class="latest_text">
-                        <h2><a href="<?php echo esc_url($url); ?>" title="<?php echo $title; ?>"><?php echo $title; ?></a></h2>
+                        <h2><a href="<?php echo ($url); ?>" title="<?php echo $title; ?>"><?php echo $title; ?></a></h2>
                         </div>
                     </div>
                     <?php if ($c == 1)  echo '</div><div class="row">'; ?>
