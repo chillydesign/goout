0cb9764280f5b392dea640a57b063d3acc54f03b
Charles Harvey <harvey.charles@gmail.com>
2017-12-12 10:20:36 +0000

    redid archive news

--- partials/archive-news.php
+++ partials/archive-news.php
@@ -1,77 +1,26 @@
-
-
 <?php $pp = 0; if (have_posts()):  ?>
-    <?php while (have_posts()) : the_post(); ?>
-
-  <?php if ($pp == 0){ ?>
-    <div class="latest_articles">
-            <div class="row">
-                <div class="col-sm-8 larger_article_image">
-
-                    <div class="latest_article">
-                      <?php $image = ( has_post_thumbnail()) ? thumbnail_of_post_url(get_the_ID(),  'medium') : ''; ?>
-                      <a  class="latest_image" href="<?php the_permalink(); ?>" style="background-image:url('<?php echo $image; ?>');" title="<?php the_title(); ?>"></a>
-                      <div class="latest_text">
-
-                          <h2><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
-
-                          <?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
-
-                      </div>
-                    </div>
-                <div class="col-sm-4 ">
-
-                                    </div>
-
-            </div>
-
-            <div class="row">
-                <div class="col-sm-4"></div>
-                <div class="col-sm-4"></div>
-                <div class="col-sm-4"></div>
-            </div>
-
-
-        </div>
-  <?php }  else {?>
-
     <div class="latest_articles">
         <div class="row">
-
-
-                <div class=" latest_article col-sm-4">
-                    <!-- article -->
-                    <article >
-
-
+            <?php while (have_posts()) : the_post(); ?>
+                <?php $col_class = ( $pp  % 5 == 0 ) ? 'col-sm-8' : 'col-sm-4' ?>
+                <div class="     latest_article latest_article_no_fixed_height <?php echo $col_class; ?>">
+                    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                        <?php //$link = get_field('link');  ?>
+                        <?php $link = get_permalink(); ?>
+                        <?php $title = get_the_title(); ?>
                         <?php $image = ( has_post_thumbnail()) ? thumbnail_of_post_url(get_the_ID(),  'medium') : ''; ?>
-                        <a  class="latest_image" href="<?php the_permalink(); ?>" style="background-image:url('<?php echo $image; ?>');" title="<?php the_title(); ?>"></a>
+                        <a  class="latest_image" href="<?php echo ($link); ?>" style="background-image:url('<?php echo $image; ?>');" title="<?php echo $title; ?>"></a>
                         <div class="latest_text">
 
-                            <h2><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></h2>
+                            <h2><a href="<?php echo $link; ?>" title="<?php echo $title; ?>"><?php echo $title; ?></a></h2>
 
-                            <?php html5wp_excerpt('html5wp_index'); // Build your custom callback length in functions.php ?>
+                            <?php html5wp_excerpt('html5wp_index');  ?>
 
                         </div>
-
                     </article>
-                    <!-- /article -->
                 </div>
-                <?php if ($pp % 3 == 2)  echo '</div><!--  END OF ROW --><div class="row">'; ?>
-
-
-
+                    <?php if ($pp % 5 == 1 || $pp % 5 == 4) echo '</div><div class="row">'; ?>
+                <?php $pp++; endwhile; ?>
         </div><!--  END OF ROW -->
     </div> <!-- END OF LATEST ARTICLES -->
-
-<?php }  ?>
-  <?php $pp++; endwhile; ?>
-<?php else: ?>
-
-    <!-- article -->
-    <article>
-        <h2><?php _e( 'Sorry, nothing to display.', 'webfactor' ); ?></h2>
-    </article>
-    <!-- /article -->
-
 <?php endif; ?>
