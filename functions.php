8428f804b38759a4b93760b18cca6d865e0a1a6f
Mélissa Rissel <rissel.melissa@gmail.com>
2017-12-17 22:17:49 +0000

    styling fixes, added custom editor styles to admin

--- functions.php
+++ functions.php
@@ -819,6 +819,56 @@ function social_meta_properties(){
 }
 
 
+
+
+function wpb_mce_buttons_2($buttons) {
+    array_unshift($buttons, 'styleselect');
+    return $buttons;
+}
+add_filter('mce_buttons_2', 'wpb_mce_buttons_2');
+function my_mce_before_init_insert_formats( $init_array ) {
+// Define the style_formats array
+    $style_formats = array(
+/*
+* Each array child is a format with it's own settings
+* Notice that each array has title, block, classes, and wrapper arguments
+* Title is the label which will be visible in Formats menu
+* Block defines whether it is a span, div, selector, or inline style
+* Classes allows you to define CSS classes
+* Wrapper whether or not to add a new block-level element around any selected elements
+*/
+        array(
+            'title' => 'Encadré',
+            'block' => 'div',
+            'classes' => 'encadre',
+            'wrapper' => true,
+
+        ),   array(
+              'title' => 'Bouton',
+              'block' => 'div',
+              'classes' => 'button_article',
+              'wrapper' => true,
+
+          )
+    );
+    // Insert the array, JSON ENCODED, into 'style_formats'
+    $init_array['style_formats'] = json_encode( $style_formats );
+    return $init_array;
+}
+// Attach callback to 'tiny_mce_before_init'
+add_filter( 'tiny_mce_before_init', 'my_mce_before_init_insert_formats' );
+
+function my_theme_add_editor_styles() {
+    add_editor_style( 'custom-editor-styles.css' );
+}
+add_action( 'init', 'my_theme_add_editor_styles' );
+
+
+
+
+
+
+
 include('functions_custom_post_types.php');
 include('functions_abonne_form.php');
 
