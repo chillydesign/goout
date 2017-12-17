6b021cd83670b5c16a87c3984bf38ff394d84a97
Charles Harvey <harvey.charles@gmail.com>
2017-12-17 19:02:19 +0000

    added advert picture to  section loop

--- sections/advert_and_text.php
+++ sections/advert_and_text.php
@@ -1,5 +1,8 @@
 <?php $content =  get_sub_field('content'); ?>
-<?php $advert = false  ?>
+<?php $adverts = get_posts( array(
+    'post_type' =>  'publicite',
+    'posts_per_page' => 1
+));  ?>
 
 <div class="container">
     <div class="row">
@@ -11,8 +14,12 @@
         </div>
         <div class="col-sm-3 col-sm-pull-8">
 
+            <?php foreach ( $adverts as $advert ) :  setup_postdata( $advert ); ?>
+                <?php $image = thumbnail_of_post_url($advert->ID, 'medium' ); ?>
+                <?php $lien = get_field('lien', $advert->ID); ?>
+                <a href="<?php echo esc_url($lien); ?>" class="singl_dvr_cont" style="background-image:url('<?php echo $image; ?>')"><?php the_title(); ?></a>
+            <?php endforeach; wp_reset_postdata(); ?>
 
-            <div style="background:grey;height:200px;width:100%;padding:10px"> ADVERT </div>
 
         </div>
 
