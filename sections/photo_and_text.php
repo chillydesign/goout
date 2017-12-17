3aa0c838f25705076348e4abbe2d3db24f8887e1
Charles Harvey <harvey.charles@gmail.com>
2017-12-17 22:00:15 +0000

    add sidebar image caption and link

--- sections/photo_and_text.php
+++ sections/photo_and_text.php
@@ -1,6 +1,7 @@
 <?php $image =  get_sub_field('image'); ?>
 <?php $content =  get_sub_field('content'); ?>
 <?php $image_position = get_sub_field('image_position'); ?>
+<?php $image_lien = get_sub_field('image_lien'); ?>
 
 <?php
 
@@ -11,7 +12,7 @@
 // };
 
 
-    $classes = [ 'col-sm-8 col-sm-push-4', 'col-sm-3 col-sm-pull-8'  ];
+$classes = [ 'col-sm-8 col-sm-push-4', 'col-sm-3 col-sm-pull-8'  ];
 
 
 ?>
@@ -20,19 +21,28 @@
     <div class="row">
         <div class="<?php echo $classes[0]; ?>">
 
-                <?php echo $content; ?>
+            <?php echo $content; ?>
 
 
         </div>
         <div class="<?php echo $classes[1]; ?>">
 
 
-            <img class="sidebar_photo" src="<?php echo $image['url']; ?>" alt="" />
+            <?php if ($image_lien) : ?><a class="sidebar_photo_link"  target="_blank" href="<?php echo esc_url( $image_lien ); ?>"><?php endif; ?>
+                <figure>
+                    <img class="sidebar_photo" src="<?php echo $image['url']; ?>" alt="" />
+                    <?php if($image['caption'] !== '' ): ?>
+
+                            <p class="sidebar_caption"><?php echo $image['caption']; ?></p>
+
+                    <?php endif ?>
+                </figure>
+                <?php if ($image_lien) : ?></a><?php endif; ?>
 
 
 
 
-        </div>
+            </div>
 
-    </div> <!-- END OF ROW -->
-</div><!--  END OF CONTAINER -->
+        </div> <!-- END OF ROW -->
+    </div><!--  END OF CONTAINER -->
